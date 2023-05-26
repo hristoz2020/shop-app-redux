c5d189a31f8ca38b4a25eb42e78d1beb54d8c0e2
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-05-26 16:19:31 +0300

    add like button to product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,24 +6,27 @@ const ProductCard = ({ products }) => {
 			title.length > 31 ? title.slice(0, 50).concat("...") : title;
 		return (
 			<div
-				className="card d-flex align-items-center border w-25 m-2 bg-light"
+				className="card border w-25 m-2 bg-light"
 				key={id}
 			>
-				<div className="h-50">
+				<div className="h-50 m-auto">
 					<img
 						src={image}
 						alt={productTitle}
-						className="product-img card-title"
+						className="product-img"
 					/>
 				</div>
-				<div className="card-body">
+				<div className="card-body p-0">
 					<p className="text-dark">{productTitle}</p>
 					<p className="text-dark">$ {price}</p>
 					<p className="text-dark">{category}</p>
 				</div>
-				<Link to={`/products/${id}`} className="btn btn-primary">
-					Details
-				</Link>
+				<div className="d-flex justify-content-between w-100 p-2">
+					<Link to={`/products/${id}`} className="btn btn-primary">
+						Details
+					</Link>
+					<button className="btn btn-primary">Like</button>
+				</div>
 			</div>
 		);
 	});
