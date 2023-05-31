9caa3b0c661356c0e98fde44ead75dfcfddc8af9
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-05-31 17:43:52 +0300

    change design of cart

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const CartProduct = ({ product }) => {
 	let productTitle =
 		product.title.length > 31
@@ -5,16 +7,47 @@ const CartProduct = ({ product }) => {
 			: product.title;
 
 	return (
-		<div className="d-flex border w-100 bg-light">
-			<img
-				src={product.image}
-				alt={productTitle}
-				className="product-img"
-			/>
-			<p className="text-dark">{productTitle}</p>
-			<p className="text-dark">$ {product.price}</p>
-			<p className="text-dark">{product.category}</p>
-		</div>
+		<tbody>
+			<tr>
+				<td class="p-4">
+					<div class="media align-items-center">
+						<img
+							src={product.image}
+							class="d-block ui-w-40 ui-bordered mr-4 product-img"
+							alt=""
+						/>
+						<div class="media-body">
+							<Link
+								to={`/products/${product.id}`}
+								class="d-block text-dark"
+							>
+								{productTitle}
+							</Link>
+							<small>
+								<span class="text-muted">Category: </span>{" "}
+								{product.category}
+							</small>
+						</div>
+					</div>
+				</td>
+				<td class="text-right font-weight-semibold align-middle p-4">
+					{product.price} BGN
+				</td>
+				<td class="align-middle p-4">
+					<input
+						type="text"
+						class="form-control text-center"
+						value="2"
+					/>
+				</td>
+				<td class="text-right font-weight-semibold align-middle p-4">
+					/total/
+				</td>
+				<td class="text-center align-middle px-0">
+					<i class="fa-solid fa-x text-danger"></i>
+				</td>
+			</tr>
+		</tbody>
 	);
 };
 
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import CartProduct from "../components/CartProduct";
 
 const Cart = () => {
@@ -8,6 +9,7 @@ const Cart = () => {
 	const cartProducts = useSelector(
 		(state) => state.cartProducts.cartProducts
 	);
+	const navigate = useNavigate();
 
 	const currentModeText = isdarkMode ? darkModeOn.text : darkModeOff.text;
 
@@ -15,9 +17,55 @@ const Cart = () => {
 		<div className="min-height">
 			<h1 className={`text-center ${currentModeText}`}>Cart</h1>
 			<div>
-				{cartProducts.map((product) => (
-					<CartProduct product={product} key={product.id} />
-				))}
+				<div class="table-responsive">
+					<table class="table table-bordered m-0">
+						<thead>
+							<tr>
+								<th class="text-center py-3 px-4">
+									Product Name &amp; Details
+								</th>
+								<th class="text-right py-3 px-4">Price</th>
+								<th class="text-center py-3 px-4">Quantity</th>
+								<th class="text-right py-3 px-4">Total</th>
+								<th class="text-center align-middle py-3 px-0">
+									<button>
+										<i class="fa fa-trash"></i>
+									</button>
+								</th>
+							</tr>
+						</thead>
+						{cartProducts.map((product) => (
+							<CartProduct product={product} key={product.id} />
+						))}
+					</table>
+				</div>
+				<div class="d-flex  justify-content-end pb-4">
+					<div>
+						<div>
+							<button
+								type="button"
+								class="btn"
+								onClick={() => navigate(-1)}
+							>
+								Back to shopping
+							</button>
+							<button
+								type="button"
+								class="btn btn-lg btn-primary mt-2"
+							>
+								Checkout
+							</button>
+						</div>
+						<div class="mt-1">
+							<label class="text-muted font-weight-normal m-0">
+								Total price
+							</label>
+							<div class="text-large">
+								<strong>?????</strong>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
