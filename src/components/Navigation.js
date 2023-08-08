d602c8c93548e0a9d1ea99d8f8af61c961be33c8
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-08-08 12:25:06 +0300

    Refactored code

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,7 +13,6 @@ const Header = () => {
 		: darkModeOff.background;
 
 	const currentModeText = isdarkMode ? darkModeOn.text : darkModeOff.text;
-	const navLinkClassName = `nav-link ${currentModeText} m-1 mt-2 border-start h-25 ps-2`;
 
 	const switchDarkMode = () => {
 		isdarkMode
@@ -21,42 +20,54 @@ const Header = () => {
 			: dispatch(handledarkMode(true));
 	};
 
+	const routes = [
+		{
+			id: 0,
+			path: "/",
+			title: "Home",
+			className: `nav-link ${currentModeText} m-1 mt-2 h-25 ps-2`
+		},
+		{
+			id: 1,
+			path: "/products",
+			title: "Products",
+			className: `nav-link ${currentModeText} m-1 mt-2 border-start h-25 ps-2`
+		},
+		{
+			id: 2,
+			path: "/categories",
+			title: "Categories",
+			className: `nav-link ${currentModeText} m-1 mt-2 border-start h-25 ps-2`
+		},
+		{
+			id: 3,
+			path: "/favorites",
+			title: "Favorites",
+			className: `nav-link ${currentModeText} m-1 mt-2 border-start h-25 ps-2`
+		},
+		{
+			id: 4,
+			path: "/cart",
+			title: "Cart",
+			className: `nav-link ${currentModeText} m-1 mt-2 border-start h-25 ps-2`
+		},
+	];
+
 	return (
 		<div className={`navbar ${currentModeBackground} border-bottom mb-4`}>
 			<div className="d-flex align-items-center justify-content-between w-100">
-					<h5 className={`${currentModeText} pt-2 ps-2`}>FakeShop</h5>
+				<h5 className={`${currentModeText} pt-2 ps-2`}>FakeShop</h5>
 				<div className="d-flex">
+					{routes.map((item) => (
+						<Link
+							to={item.path}
+							key={item.id}
+							className={item.className}
+						>
+							{item.title}
+						</Link>
+					))}
 
-					<Link
-						to="/"
-						className={`nav-link ${currentModeText} p-1 pt-2 ps-2`}
-					>
-						Home
-					</Link>
-					<Link
-						to="/products"
-						className={navLinkClassName}
-					>
-						Products
-					</Link>
-					<Link
-						to="/categories"
-						className={navLinkClassName}
-					>
-						Categories
-					</Link>
-					<Link
-						to="/favorites"
-						className={navLinkClassName}
-					>
-						Favorites
-					</Link>
-					<Link
-						to="/cart"
-						className={`nav-link ${currentModeText} m-1 mt-2 border-start border-end h-25 ps-2 pe-2`}
-					>
-						Cart
-					</Link>
 					<div className="form-check form-switch m-1 mt-2 h-25">
 						<input
 							checked={isdarkMode}
